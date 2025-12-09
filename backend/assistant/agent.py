03a6213c7fe7a8d8db7bcc7532b53a41e065e0c5
anouillz <anagoncalves7@icloud.com>
2025-12-09 10:22:29 +0100

    feat: enhance chatbot response handling with improved context management

--- backend/assistant/agent.py
+++ backend/assistant/agent.py
@@ -3,7 +3,8 @@ from langgraph.graph import StateGraph
 from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.checkpoint.memory import MemorySaver
 from langchain_ollama import ChatOllama
-from langchain_core.messages import SystemMessage
+from langchain_core.messages import SystemMessage, HumanMessage
+from langgraph.graph.message import add_messages
 from langsmith import traceable
 from dotenv import load_dotenv
 
@@ -15,32 +16,46 @@ load_dotenv()
 llm = ChatOllama(
     model=OLLAMA_MODEL,
     base_url=OLLAMA_ADDRESS,
-    temperature=0.1, 
+    temperature=0, 
 )
-# link tools with llm
+
+#link tools with llm
 llm_with_tools = llm.bind_tools(tools)
 
 class AgentState(TypedDict):
-    messages: Annotated[list, "add_messages"]
+    messages: Annotated[list, add_messages]
 
 @traceable(name="Chatbot Node")
 def chatbot_node(state: AgentState):
     messages = state["messages"]
-    current_prompt = SYSTEM_PROMPT
-    
-    # check if last message is from a tool 
-    if len(messages) > 0 and messages[-1].type == "tool":
-        current_prompt += "\n\nINFORMATION IMPORTANTE : Tu viens de recevoir les données de l'outil ci-dessus. UTILISE-LES pour répondre à l'utilisateur MAINTENANT. Ne pose pas de questions."
-
-    sys_msg = SystemMessage(content=current_prompt)
-    
-    # build context 
+    sys_msg = SystemMessage(content=SYSTEM_PROMPT)
     full_context = [sys_msg] + messages
     
-    response = llm_with_tools.invoke(full_context)
+    if len(messages) > 0 and hasattr(messages[-1], 'type') and messages[-1].type == "tool":
+        
+        last_human_msg = None
+        for m in reversed(messages):
+            if m.type == "human":
+                last_human_msg = m.content
+                break
+        
+        if last_human_msg:
+            reminder_text = f"""
+            STOP ! Ne cherche plus. J'ai les infos. Pas besoin de t'excuser. 
+            Réponds UNIQUEMENT à ma question : "{last_human_msg}".
+            Ignore les autres vins du texte s'ils ne correspondent pas.
+            """
+            full_context.append(HumanMessage(content=reminder_text))
+        
+        # use llm without tools to answer question with retrieved context
+        response = llm.invoke(full_context)
+        
+    else:
+        response = llm_with_tools.invoke(full_context)
+
     return {"messages": [response]}
 
-# langgraph
+#langgraph
 workflow = StateGraph(AgentState)
 workflow.add_node("chatbot", chatbot_node)
 workflow.add_node("tools", ToolNode(tools))
